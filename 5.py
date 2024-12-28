33a02ae67741da94e5239c919416cad3591b1f29
lgf <liugong.feng@aliyun.com>
2024-12-28 19:39:33 +0800

    Code fun.

---
+++ 5.py
@@ -0,0 +1,16 @@
+'''backtest
+start: 2023-01-01 00:00:00
+end: 2024-12-18 15:00:00
+period: 1d
+basePeriod: 1d
+exchanges: [{"eid":"Futures_CTP","currency":"FUTURES"},{"eid":"Futures_CTP","currency":"FUTURES"}]
+'''
+
+def main():
+    while True:
+        if exchange.IO("status"):
+            ret = exchange.SetContractType("MA888")
+            Log("Info: ", ret)
+            breal
+        else:
+            LogStatus(_D(), "DisConnected!")
